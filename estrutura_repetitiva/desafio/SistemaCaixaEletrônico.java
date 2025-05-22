c6f373b5383234fd390e15240cf511a5fea7ee29
Michel Morais <valentimmichel@gmail.com>
2025-05-22 14:10:22 -0300

    Melhorias no sistema Caixa eletrônico

--- estrutura_repetitiva/desafio/SistemaCaixaEletrônico.java
+++ estrutura_repetitiva/desafio/SistemaCaixaEletrônico.java
@@ -8,42 +8,31 @@ public class SistemaCaixaEletrônico {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
-
+		
+		final double SALDO_INICIAL_PADRAO = 1000.00;
+		
 		double saldoInicial = 1000.00;
-		double saque = 0;
-		double deposito = 0;
 		double saqueTotal = 0;
 		double depositoTotal = 0;
-		double saldoFinal = saldoInicial;
-
-		System.out.println("Menu do Sistema:");
-		System.out.println("1 - Saque");
-		System.out.println("2 - Depósito");
-		System.out.println("3 - Consulta de saldo");
-		System.out.println("4 - Sair");
-		System.out.print("Informe a opção: ");
-
+		double saldoFinal = SALDO_INICIAL_PADRAO;
+		
+		exibirMenu();
+		
 		int opcao = sc.nextInt();
 		
 		while(opcao != 4) {
 			
 			if(opcao == 1) {
-				System.out.print("Valor de saque: ");
-				saque = sc.nextDouble();
-				if(saque > saldoFinal) {
-					System.out.println("Valor insuficiente!");
-				} else {
-					saldoFinal -= saque;
-					saqueTotal += saque;
-				}
+				double[] resultado = operacaoSacar(sc, saldoFinal, saqueTotal);
+				saldoFinal = resultado[0];
+				saqueTotal = resultado[1];
+
 			} else if(opcao == 2) {
-				System.out.print("Valor do depósito: ");
-				deposito = sc.nextDouble();
-				saldoFinal += deposito;
-				depositoTotal += deposito;
+				double[] resultado = operacaoDepositar(sc, saldoFinal, depositoTotal);
+				saldoFinal = resultado[0];
+				depositoTotal = resultado[1];
 			} else if (opcao == 3) {
-				double consultaSaldo =  saldoFinal;
-				System.out.printf("Saldo atual R$ %.2f%n", consultaSaldo);
+				operacaoConsultarSaldo(saldoFinal);
 			} else {
 				System.out.println("Opção inválida!");
 			}
@@ -51,15 +40,54 @@ public class SistemaCaixaEletrônico {
 			opcao = sc.nextInt();
 		}
 		
-		System.out.println("Programa encerrado!");
+		imprimirRelatorio(saldoInicial, depositoTotal, saqueTotal, saldoFinal);
+
+		sc.close();
+
+	}
+	
+	public static void exibirMenu() {
+		System.out.println("Menu do Sistema:");
+		System.out.println("1 - Saque");
+		System.out.println("2 - Depósito");
+		System.out.println("3 - Consulta de saldo");
+		System.out.println("4 - Sair");
+		System.out.print("Informe a opção: ");
+	}
+	
+	//Função que retorna um array
+	public static double[] operacaoSacar(Scanner sc, double saldoFinal, double saqueTotal) {
+		System.out.print("Valor de saque: ");
+		double saque = sc.nextDouble();
+		if(saque > saldoFinal) {
+			System.out.println("Valor insuficiente!");
+		} else {
+			saldoFinal -= saque;
+			saqueTotal += saque;
+		}
+		return new double[] {saldoFinal, saqueTotal};
+	}
+	
+	public static double[] operacaoDepositar(Scanner sc, double saldoFinal, double depositoTotal) {
+		System.out.print("Valor do depósito: ");
+		double deposito = sc.nextDouble();
+		saldoFinal += deposito;
+		depositoTotal += deposito;
+		return new double[] {saldoFinal, depositoTotal};
+	}
+	
+	public static void operacaoConsultarSaldo(double saldoFinal) {
+		double consultaSaldo =  saldoFinal;
+		System.out.printf("Saldo atual R$ %.2f%n", consultaSaldo);
+	}
+	
+	public static void imprimirRelatorio(double saldoInicial, double depositoTotal, double saqueTotal, double saldoFinal) {
+		System.out.println("\nPrograma encerrado!");
 		
 		System.out.println("\nRelatório de saques e depósitos");
 		System.out.printf("Saldo Inicial em conta R$ %.2f%n", saldoInicial);
 		System.out.printf("Depósito total em conta R$ %.2f%n", depositoTotal);
 		System.out.printf("Saque total em conta R$ %.2f%n", saqueTotal);
 		System.out.printf("Saldo final em conta R$ %.2f%n", saldoFinal);
-
-		sc.close();
-
 	}
 }
