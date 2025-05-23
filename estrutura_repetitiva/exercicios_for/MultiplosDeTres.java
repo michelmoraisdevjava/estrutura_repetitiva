2d40290cf7245b3b510422c45297943636c08a06
Michel Morais <valentimmichel@gmail.com>
2025-05-23 13:31:56 -0300

    Multiplos de 3 com Output na horizontal

--- estrutura_repetitiva/exercicios_for/MultiplosDeTres.java
+++ estrutura_repetitiva/exercicios_for/MultiplosDeTres.java
@@ -5,18 +5,10 @@ public class MultiplosDeTres {
 
 	public static void main(String[] args) {
 		/*✅ Primeira abordagem: Usando incremento de 3 (i += 3)*/
-		for(int i = 3; i < 30; i += 3) {
-			System.out.println(i);
-		}
-		/*✔ Vantagens:
-		 * Mais eficiente, pois evita verificações desnecessárias.
-		 * O loop percorre apenas os múltiplos de 3 diretamente.
-		 * Código mais limpo e legível.*/
-		
-		System.out.println("######");
-		for(int i = 3; i < 30; i++) {
-			if(i % 3 == 0) {
-				System.out.println(i);
+		for(int i = 0; i <= 30; i += 3) {
+			System.out.print(i);
+			if(i <= 30 -1) {
+				System.out.print(", ");
 			}
 		}
 
