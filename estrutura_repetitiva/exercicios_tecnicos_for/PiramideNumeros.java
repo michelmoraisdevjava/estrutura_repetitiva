0bbfad93e81cd0f2f45ab2713d7df739283a7fa8
Michel Morais <valentimmichel@gmail.com>
2025-05-25 17:24:30 -0300

    Pirâmide de Números

---
+++ estrutura_repetitiva/exercicios_tecnicos_for/PiramideNumeros.java
@@ -0,0 +1,31 @@
+package exercicios_tecnicos_for;
+
+import java.util.Scanner;
+
+/*Crie um programa que exiba uma pirâmide numérica 
+ * com base em um número informado pelo usuário.*/
+
+public class PiramideNumeros {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Informe o número: ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				imprimir(i);
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+
+	}
+	
+	public static void imprimir(int i) {
+		System.out.print(i);
+	}
+
+}
