bce17949693036fdb5741b4af7495c2ac224a431
Michel Morais <valentimmichel@gmail.com>
2025-05-27 15:23:14 -0300

    Imprimir múltiplos de 5 entre 0 e 50

---
+++ estrutura_repetitiva/do_while/ImprimirMultiplosDeCinco.java
@@ -0,0 +1,20 @@
+package do_while;
+
+/*Imprima os múltiplos de 5 entre 0 e 50 utilizando do-while.*/
+public class ImprimirMultiplosDeCinco {
+
+	public static void main(String[] args) {
+
+		int numeros = 0;
+
+		do {
+			
+			if(numeros % 5 == 0) {
+				System.out.println(numeros);
+			}
+			numeros++;
+		} while (numeros <= 50 );
+		
+	}
+
+}
