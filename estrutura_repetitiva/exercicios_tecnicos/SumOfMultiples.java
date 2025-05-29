c10593353523e588b3f427a22764a3d404588f6a
Michel Morais <valentimmichel@gmail.com>
2025-05-28 21:33:46 -0300

    Soma de Múltiplos

--- estrutura_repetitiva/exercicios_tecnicos/SumOfMultiples.java
+++ estrutura_repetitiva/exercicios_tecnicos/SumOfMultiples.java
@@ -11,7 +11,20 @@ public class SumOfMultiples {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Digite um número: ");
+		int n = sc.nextInt();
+		int i = 1;
 		
+		int somaMultiplos = 0;
+		while(i < n) {
+			if(i % 3 == 0 || i % 5 == 0) {
+				System.out.print(i + " ");
+				somaMultiplos += i;
+			}
+			i++;
+		}
+		System.out.println();
+		System.out.printf("A soma dos múltiplos de 3 e de 5: %d%n", somaMultiplos);
 		
 		sc.close();
 
